5c3825a8ee8f66bde368fa1dd825e7ec3a051756
Delawit12 <131721430+Delawit12@users.noreply.github.com>
2024-06-01 05:58:52 +0300

    file&folder structure

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { Route,Routes } from "react-router-dom"
+import "./global.css"
+import SignupForm from "../src/"
+const App = () => {
+  return (
+    // <main className="flex h-screen">
+    //     <Routes>
+    //         {/* public routes */}
+    //          <Route path="/sign-in" element={<SigninForm />}/>
+    //          <Route path="/sign-in" element={<SignupForm />}/>
+    //         {/* private routes */}
+    //         <Route index element={<Home/>}/>
+    //     </Routes>
+    // </main>
+  )
+}
+
+export default App
